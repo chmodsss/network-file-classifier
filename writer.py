87f5485a03904a8f9acf14e8a0f977587caf3268
Sivasurya Santhanam <sivasurya.in@gmail.com>
2025-09-19 18:00:59 +0530

    docs added

--- writer.py
+++ writer.py
@@ -8,11 +8,19 @@ logger = logging.getLogger(__name__)
 
 
 def write_results_to_file(data):
+    """
+    Write classification results to a timestamped JSON file in the output directory.
+
+    Args:
+        data (list): List of classification results to write.
+
+    Returns:
+        str: Path to the output JSON file.
+    """
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     op_filename = f"classification_output_{timestamp}.json"
     target_file = OUTPUT_DIR + op_filename
 
-    # Write JSON results to file
     with open(target_file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 
